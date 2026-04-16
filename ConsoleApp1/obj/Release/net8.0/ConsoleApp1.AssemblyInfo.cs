81ea2cde9be477be8a71ca3e7e9de2f4a96c7900
ab0715997 <ab0715997@gmail.com>
2026-04-16 16:44:48 +0200

    Updated Todo

--- ConsoleApp1/obj/Release/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Release/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8503b28303940a1fb8bba823e0a32ed5abf722b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
